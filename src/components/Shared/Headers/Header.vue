85d0ec1e5b630baa3cb1dcb969b8f86d1488f3b6
Ankitsingh Bisht <ankitsingh.bisht@indxtechnology.com>
2024-03-22 16:12:16 +0530

    init commit

---
+++ src/components/Shared/Headers/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="head-container ">
+        <p>Just Do It </p>
+
+    </div>
+</template>
+
+
+<style scoped>
+.head-container {
+    color: white;
+    font-size: 4.5rem;
+    font-weight: bolder;
+}
+</style>
