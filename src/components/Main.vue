addd0b27db7606691a919188d4be8bf7b2031c58
Ankitsingh Bisht <ankitsingh.bisht@indxtechnology.com>
2024-03-25 19:28:28 +0530

    Tailwind setup props learnt

---
+++ src/components/Main.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps({
+    toDoList: Array
+});
+
+
+let taskName: string = '';
+let selectedDate: string = '';
+const handleKeyPress = (e: any) => {
+    console.log(props.toDoList);
+    console.log(taskName);
+    console.log(selectedDate);
+}
+
+</script>
+
+<template>
+    <div class="input-area flex justify-center">
+        <input type='text' class="to-do-text-box h-14 w-2/3" placeholder="Enter Text + Enter" v-model="task"
+            @keypress.enter="handleKeyPress" bind:task /> <input type="date" v-model="selectedDate" class="h-14 w-28">
+    </div>
+</template>
+
+
+<style scoped>
+input[type="text"] {
+    font-size: 25px;
+}
+</style>
